8216db1e78e7db59ead5cf2cef0bf25667013f0d
Twirlbug <twirlbug@gmail.com>
2015-10-02 01:20:24 -0500

    adding more

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,16 +14,16 @@ var role = "Information Technology";
  
  //$("#main").append(skills);
  
-/* var bio= {
+ var bio= {
    "name":"Nicole",
-   "role":role,
+   "role":"Information Technology",
    "contacts":{
       "email":"twirlbug@gmail.com",
       "github":"Twirlbug"},
    "biopic":"https://scontent.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11709736_837723032983625_8493569490743005868_n.jpg",
-   "skills": skills
+  // "skills": skills
  };
-*/
+
 var work= {};
 work.position= "student worker";
 work.employer="ATS";

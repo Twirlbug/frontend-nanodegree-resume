ea26b7f0a07c5f3f0e138d10755099ff3b7b0d54
Twirlbug <twirlbug@gmail.com>
2015-10-01 21:52:26 -0500

    added role and name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- 
- $("#main").append("Nicole Geiger");
+ var name = "Nicole Geiger";
+ var role = "Information Technology"
+ //$("#main").append("Nicole Geiger");
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ $("#header").append(formattedName);
+ var formattedRole = HTMLheaderName.replace("%data%", role);
+ $("#header").append(formattedRole);

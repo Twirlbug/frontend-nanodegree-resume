19dd5c8669c9c09c2fe3f05d9be02b8fda7be993
Twirlbug <twirlbug@gmail.com>
2015-10-01 22:04:24 -0500

    order

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var name = "Nicole Geiger";
- var role = "Information Technology"
- //$("#main").append("Nicole Geiger");
  var formattedName = HTMLheaderName.replace("%data%", name);
- $("#header").prepend(formattedName);
+
+ //$("#main").append("Nicole Geiger");
+
  var formattedRole = HTMLheaderName.replace("%data%", role);
+  var role = "Information Technology"
  $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);

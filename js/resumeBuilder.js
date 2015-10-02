bebe68cf31224e3f5ff82ff0b506d32613a46361
Twirlbug <twirlbug@gmail.com>
2015-10-02 01:14:19 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,34 +1,34 @@
-var role = "Information Technology";
-var skills = ["Java", "C", "Adobe CS6 Suite", "Microsoft Suite"];
-
- /*var bio= {
-   "name":"Nicole Geiger",
-   "role":role,
-   "contacts":{
-      "email":"twirlbug@gmail.com",
-      "github":"Twirlbug"
-    
-   },
-   "biopic":"https://scontent.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11709736_837723032983625_8493569490743005868_n.jpg",
-   "welcomeMessage": "Greetings",
-   "skills": skills
- };*/
- 
-var formatedName=HTMLheaderName.replace("%data%", "Nicole Geiger");
-$("#header").append(formatedName);
-var formatedRole=HTMLheaderRole.replace("%data%", role);
-$("#header").append(formatedRole);
 /*
-var work= {
- "jobs":[
-  {
-"position": "student worker";
-"employer"="ATS";
-"employerTitle"="boss";
-"years"=3;
-}
-]
-};
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ var formattedName = HTMLheaderName.replace("%data%", "Nicole Geiger");
+
+ //$("#main").append("Nicole Geiger ");
+var role = "Information Technology";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+  
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ 
+ var skills = ["Java", "C", "Adobe CS6 Suite", "Microsoft Suite"];
+ 
+ //$("#main").append(skills);
+ 
+ var bio= {
+   "name"="Nicole",
+   "role"=role,
+   "contacts"={
+      "email"="twirlbug@gmail.com",
+      "github"="Twirlbug"},
+   "biopic"="https://scontent.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11709736_837723032983625_8493569490743005868_n.jpg?oh=e87fec3462292493c7dfdaea0f758b87&oe=56918832",
+   "welcomeMessage"= "Greetings",
+   "skills"= skills
+ };
+
+var work= {};
+work.position= "student worker";
+work.employer="ATS";
+work.years=3;
 
 var education={
     "schools": [
@@ -40,42 +40,4 @@ var education={
         }
     ]
 }
-
-
-var projects={
- "project":[
-  {
-     "name":"Tennis App",
-     "type":"Android App"
-  }
-  {
-   "name": "Hello World",
-   "type": "Java"
-  }
-  ]
-}
-
-
-if(bio.skills.length>0){
- $("#header").append(HTMLskillsStart);
- var formattedSkill=HTMLskills.replace("%data%", bio.skills[0]);
- $("#skills").append(formattedSkill);
- formattedSkill=HTMLskills.replace("%data%", bio.skills[1]);
- $("#skills").append(formattedSkill);
- formattedSkill=HTMLskills.replace("%data%", bio.skills[2]);
- $("#skills").append(formattedSkill);
- formattedSkill=HTMLskills.replace("%data%", bio.skills[3]);
- $("#skills").append(formattedSkill);
-}
-*/
-
-
-for (jobs in work.jobs){
- $(#workExperience).append(HTMLworkStart);
- var formatEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-  var formatTitle = HTMLworkTitle.replace("%data%", work.jobs[job].position);
-   var formatEmpTitle = formatEmployer+formatTitle;
-   $(".work-entry:last").append(formatEmptitle);
-}
-
 

d7693df38deef66b1319d646b7b54222d77d659b
Twirlbug <twirlbug@gmail.com>
2015-10-01 22:06:01 -0500

    syntax

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,8 +4,8 @@ This is empty on purpose! Your code to build the resume will go here.
  var formattedName = HTMLheaderName.replace("%data%", name);
 
  //$("#main").append("Nicole Geiger");
-
+ var role = "Information Technology";
  var formattedRole = HTMLheaderName.replace("%data%", role);
-  var role = "Information Technology"
+
  $("#header").prepend(formattedRole);
  $("#header").prepend(formattedName);

abce682b1692da98fa01c16c078cd98f815aec51
Twirlbug <twirlbug@gmail.com>
2015-10-02 01:31:09 -0500

    attempt to fix skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -25,16 +25,17 @@ var role = "Information Technology";
  };
  bio.skills= skills;
  
- if(bio.skills.length > 0){
+ if(bio.skills.length > 0) {
  $("#header").append(HTMLskillsStart);
+ 
  var formattedSkill=HTMLskills.replace("%data%", bio.skills[0]);
  $("#skills").append(formattedSkill);
- formattedSkill=HTMLskills.replace("%data%", bio.skills[1]);
+ /*formattedSkill=HTMLskills.replace("%data%", bio.skills[1]);
  $("#skills").append(formattedSkill);
  formattedSkill=HTMLskills.replace("%data%", bio.skills[2]);
  $("#skills").append(formattedSkill);
  formattedSkill=HTMLskills.replace("%data%", bio.skills[3]);
- $("#skills").append(formattedSkill);
+ $("#skills").append(formattedSkill);*/
 }
 
 var work= {

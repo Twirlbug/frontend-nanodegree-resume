0bf5e982feb4da6697678daf93c8a64b1a4d445c
Twirlbug <twirlbug@gmail.com>
2015-10-02 01:42:50 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,7 +22,7 @@ var role = "Information Technology";
       "github":"Twirlbug"},
   "biopic":"https://scontent.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11709736_837723032983625_8493569490743005868_n.jpg"
   // "skills": skills
- };
+ }
  //bio.skills= skills;
 
 var work= {

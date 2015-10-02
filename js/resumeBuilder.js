259a1d28650c70d059d676ee8eaf6232b7f75bea
Twirlbug <twirlbug@gmail.com>
2015-10-01 22:02:22 -0500

    to preprend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,6 +5,6 @@ This is empty on purpose! Your code to build the resume will go here.
  var role = "Information Technology"
  //$("#main").append("Nicole Geiger");
  var formattedName = HTMLheaderName.replace("%data%", name);
- $("#header").append(formattedName);
+ $("#header").prepend(formattedName);
  var formattedRole = HTMLheaderName.replace("%data%", role);
  $("#header").prepend(formattedRole);

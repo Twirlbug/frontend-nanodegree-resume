8add3f8045a168352833e544cc06d12ed4ab95ed
Twirlbug <twirlbug@gmail.com>
2015-10-01 22:55:47 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,3 @@
-
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
@@ -10,3 +9,19 @@ var role = "Information Technology";
   
  $("#header").prepend(formattedRole);
  $("#header").prepend(formattedName);
+ 
+ var skills = ["Java", "C", "Adobe CS6 Suite", "Microsoft Suite"];
+ 
+ $("#main").append(skills);
+ 
+/* var bio= {
+   "name"="Nicole",
+   "role"=role,
+   "contacts"={
+    "email"="twirlbug@gmail.com",
+    "github"="Twirlbug"},
+   "biopic"="https://scontent.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11709736_837723032983625_8493569490743005868_n.jpg?oh=e87fec3462292493c7dfdaea0f758b87&oe=56918832",
+   "welcomeMessage"= "Greetings",
+   "skills"= skills
+ };
+*/
